8d9b98326d0469b68e555fd4f9972b62ec5ee9cb
Marc Haemmerle <mail@marchaemmerle.com>
2018-09-17 14:13:24 +0200

    Implement method search for classes

--- commands/class_doc.js
+++ commands/class_doc.js
@@ -1,28 +1,81 @@
 var $ = require('cheerio')
 var fs = require('fs')
 
-var htmlString = fs.readFileSync('index.html').toString()
-var parsedHTML = $.load(htmlString)
+const basePath = 'Documentation/INT/API';
+const baseUrl = 'https://api.unrealengine.com/INT/API';
 
-const baseLink = 'https://api.unrealengine.com/INT/API'
-var classMap = new Map()
+const classMap = new Map();
 
-parsedHTML('.memberindexitem').map(function(i, item) {
-    item = $(item);
-    item.find('span').remove();
+function parseClasses() {
+    var htmlString = fs.readFileSync(basePath + '/Classes/index.html').toString()
+    var parsedHTML = $.load(htmlString)
 
-    var className = item.text().trim();
+    parsedHTML('.memberindexitem').map(function(i, item) {
+        item = $(item);
+        item.find('span').remove();
+    
+        const className = item.text().trim();
+        const linkItem = item.find('a');
+        const hrefItem = linkItem.attr('href');
+        const dotRegex = /\.\./;
+        const cleanUrl = hrefItem.replace(dotRegex, '');
+        const docUrl = baseUrl + cleanUrl;
+    
+        classMap.set(className.toLowerCase(), docUrl);
 
-    var linkItem = item.find('a');
-    var hrefItem = linkItem.attr('href');
-    var dotRegex = /\.\./;
-    var docLink = baseLink + hrefItem.replace(dotRegex, '');
+        if(className == 'AActor') {
+            const filePath = basePath + cleanUrl;
+            const subPath = cleanUrl.replace(/index.html/g,'');
 
-    classMap.set(className.toLowerCase(), docLink);
-});
+            // console.log('filePath ' + filePath);
+            // console.log('subPath ' + subPath);
 
-htmlString = null;
-parsedHTML = null;
+            const methodMap = parseMethods(filePath, subPath);
+
+            classMap.set(className.toLowerCase() + '|methods', methodMap);
+        }
+    });
+    
+    htmlString = null;
+    parsedHTML = null;
+}
+
+function parseMethods(filePath, subPath) {
+    var htmlString = fs.readFileSync(filePath).toString()
+    var parsedHTML = $.load(htmlString)
+
+    const methodMap = new Map();
+
+    parsedHTML('.name-cell a').map(function(i, item) {
+        item = $(item);
+        const methodName = item.text().trim().replace(/\(\)/g,'');
+        const href = item.attr('href');
+        const docUrl = baseUrl + subPath + href;
+    
+        methodMap.set(methodName.toLowerCase(), {name: methodName, url: docUrl});
+    
+        // console.log('methodName ' + methodName + ', docUrl ' + docUrl)
+    });
+
+    htmlString = null;
+    parsedHTML = null;
+
+    return methodMap;
+}
+
+parseClasses();
+
+function methodsStartingWith(methodMap, str) {
+    const methods = [];
+
+    for (let methodName of methodMap.keys()) {
+        if(methodName.startsWith(str)) {
+            methods.push(methodMap.get(methodName));
+        }
+    }
+
+    return methods;
+}
 
 const { Command } = require('discord-akairo');
 
@@ -34,6 +87,9 @@ class ClassDocCommand extends Command {
                 {
                     id: 'clazz',
                     type: 'string'
+                }, {
+                    id: 'method',
+                    type: 'string'
                 }
             ]
         });
@@ -41,17 +97,41 @@ class ClassDocCommand extends Command {
 
     exec(message, args) {
         if(args.clazz !== null && args.clazz !== '') {
-            var clazz = args.clazz.toLowerCase();
+            const clazz = args.clazz.toLowerCase();
 
             if(classMap.has(clazz)) {
+                if(args.method !== null && args.method !== '') {
+                    const method = args.method.toLowerCase()
+
+                    // console.log('method supplied: ' + args.method);
+
+                    const methodMap = classMap.get(clazz + '|methods');
+
+                    if(methodMap !== undefined) {
+                        const result = methodsStartingWith(methodMap, method);
+
+                        // console.log('found ' + result.length + ' method matches');
+
+                        if(result.length == 0) {
+                            return message.reply("No method named '" + args.method + "' found on '" + args.clazz + "'.");
+                        } else if(result.length == 1) {
+                            const key = result[0].name.toLowerCase();
+                            return message.reply(methodMap.get(key).url);
+                        } else {
+                            const msg = "More than one method found for '" + args.method + "': " + result.map(item => item.name).join(', ');
+
+                            return message.reply(msg);
+                        }
+                    }
+                }
+
                 return message.reply(classMap.get(clazz));
-            }
-            else {
+            } else {
                 return message.reply("Class '" + args.clazz + "' not found.");
             }
-        } else {
-            return message.reply("Missing class name.");
         }
+        
+        return message.reply("Missing class name.");
     }
 }
 
